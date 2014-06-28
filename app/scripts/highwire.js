71820b3e98493df432366f4e13c6b310f7fea2ee
Ryan McVeigh <ryanm@newmediadenver.com>
2014-06-28 17:44:22 -0600

    added drag-n-drop and list view

--- app/scripts/highwire.js
+++ app/scripts/highwire.js
@@ -3,4 +3,25 @@ $(function() {
     e.preventDefault()
     $(this).tab('show')
   });
+  $('.draggable').draggable({
+        scroll: false,
+        revert: "invalid",
+        snap: ".search-results",
+        stack: ".col-md-3",
+        zIndex: 9999,
+        appendTo: '.drop-zone',
+        helper: 'clone'
+    });
+  $('.drop-zone').droppable({
+    accept: '.draggable',
+    drop: function (event, ui) {
+      var helperHtml = $(ui.helper).parent().html();
+      var helperList = '<li><div class="search-result">' + $(ui.helper).html() + '</div></li>';
+       $('.canvas-view').append(helperHtml);
+       $('.convas-view .search-result').removeClass('ui-draggable-dragging');
+       $('.list-view .list-view-list').append(helperList);
+       $('.list-view .list-view-list .search-result').attr('style', '');
+       $(ui.draggable).removeClass('draggable ui-draggable');
+    }
+  });
 });
