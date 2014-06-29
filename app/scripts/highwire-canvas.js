3c6c7cf046154abeb4ff1193e7d25938aab3e191
Ryan McVeigh <ryanm@newmediadenver.com>
2014-06-29 16:10:40 -0600

    working on sync events

--- app/scripts/highwire-canvas.js
+++ app/scripts/highwire-canvas.js
@@ -1,3 +1,18 @@
+//outerHTML function
+$.fn.outerHTML = function(){
+
+    // IE, Chrome & Safari will comply with the non-standard outerHTML, all others (FF) will have a fall-back for cloning
+    return (!this.length) ? this : (this[0].outerHTML || (
+      function(el){
+          var div = document.createElement('div');
+          div.appendChild(el.cloneNode(true));
+          var contents = div.innerHTML;
+          div = null;
+          return contents;
+    })(this[0]));
+
+}
+
 $(function() {
 
   //side tabs
@@ -21,7 +36,7 @@ $(function() {
   $('.drop-zone').droppable({
     accept: '.draggable',
     drop: function (event, ui) {
-      var helperHtml = $(ui.helper).parent().html();
+      var helperHtml = $(ui.helper).outerHTML();
       var helperList = '<li><div class="search-result">' + $(ui.helper).html() + '</div></li>';
        $('.drop-zone').append(helperHtml);
        $('.drop-zone .search-result').removeClass('ui-draggable-dragging');
@@ -52,6 +67,7 @@ $(function() {
     var $clickObject = $(this);
     var clickTitle = $clickObject.find('.object-title').text();
     var clickJcode = $clickObject.find('span.jCode').text();
+    var titleDataRole = $clickObject.find('.object-title').attr('data-role');
     //loop through each search result and remove active class
     $('.main-content-outlet .search-result').each(function(index, el) {
       var $thisResult = $(this);
@@ -61,6 +77,14 @@ $(function() {
     });
     $('.icon-edit a').click();
     $clickObject.addClass('active');
+    $(".main-content-outlet .droppable-list .search-result .object-title")
+      .each(function(index, el) {
+      var $thisTitleItem = $(this);
+      var thisTitleRole = $thisTitleItem.attr('data-role');
+      if (thisTitleRole == titleDataRole) {
+        $thisTitleItem.closest('.search-result').addClass('active');
+      };
+    });
     $('#edit #edit-domain').val(clickTitle);
     $('#edit #edit-jcode').val(clickJcode);
   });
@@ -81,27 +105,36 @@ $(function() {
         $thisA.removeClass('active');
       };
     });
+    $('#sync input').each(function(index, el) {
+      var $thisInput = $(this);
+      var thisId = $(this).attr('id');
+      if (thisId == enviroName) {
+        $thisInput.attr("disabled", "disabled");
+      } else {
+        $thisInput.removeAttr('disabled');
+      }
+    });
     $('.main-content-outlet .canvas-view').each(function(index, el) {
       var $thisCanvas = $(this);
       if ($thisCanvas.hasClass('drop-zone')) {
-        $thisCanvas.fadeOut().droppable('destroy').removeClass('drop-zone');
+        $thisCanvas.hide().droppable('destroy').removeClass('drop-zone');
       };
     });
     $('.main-content-outlet .list-view').each(function(index, el) {
       var $thislist = $(this);
       if ($thislist.hasClass('droppable-list')) {
-        $thislist.fadeOut().removeClass('droppable-list');
+        $thislist.hide().removeClass('droppable-list');
       };
     });
     if ($('#list-radio').closest('.btn').hasClass('active')) {
       $(listId).show();
     };
     $(listId).addClass('droppable-list');
-    $(canvasId).fadeIn().addClass('drop-zone').droppable({
+    $(canvasId).show().addClass('drop-zone').droppable({
       accept: '.draggable',
       drop: function (event, ui) {
-        var helperHtml = $(ui.helper).parent().html();
-        var helperList = '<li><div class="search-result">' + $(ui.helper).html() + '</div></li>';
+        var helperHtml = $(ui.helper).outerHTML();
+        var helperList = '<li><div class="search-result">' + $(ui.helper).outerHTML() + '</div></li>';
          $('.drop-zone').append(helperHtml);
          $('.drop-zone .search-result').removeClass('ui-draggable-dragging');
          $('.droppable-list .list-view-list').append(helperList);
@@ -113,15 +146,148 @@ $(function() {
     $clickedTab.addClass('active');
     $('.main-content-outlet .drop-zone .search-result .object-title').each(function(index, el) {
       var $thisCanvasObject = $(this),
-      $thisCanvasObjectRole = $thisCanvasObject.attr('role');
+      $thisCanvasObjectRole = $thisCanvasObject.attr('data-role');
       $('.search-results .search-result .object-title').each(function(index, el) {
         var $thisResultObject = $(this);
-        var thisResultRole = $(this).attr('role');
+        var thisResultRole = $(this).attr('data-role');
         if (thisResultRole == $thisCanvasObjectRole) {
           $thisResultObject.closest('.draggable').hide();
         };
       });
     });
   });
+
+  //clone progress bar
+  $( "input.clone-btn" ).click(function(event) {
+      /* Act on the event */
+      if ($('.main-content-outlet .drop-zone .search-result.active').length){
+      $(function() {
+      var progressbar = $( "#clone-progressbar" ),
+        progressLabel = $( "#clone-progressbar .progress-label" );
+      progressbar.fadeIn();
+      progressbar.progressbar({
+        value: false,
+        change: function() {
+          progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+        },
+        complete: function() {
+          progressbar.fadeOut();
+        }
+      });
+
+      function progress() {
+        var val = progressbar.progressbar( "value" ) || 0;
+
+        progressbar.progressbar( "value", val + 2 );
+
+        if ( val < 99 ) {
+          setTimeout( progress, 80 );
+        }
+      }
+
+      setTimeout( progress, 2000 );
+    });
+  } else {
+    alert('Please select an object from the canvas.');
+  }
+  });
+
+  //delete progress bar
+  $(function() {
+    var progressTimer,
+      progressbar = $( "#delete-progressbar" ),
+      progressLabel = $( ".delete-progress-label" ),
+      dialogButtons = [{
+        text: "Cancel Deletion",
+        click: closeDownload
+      }],
+      dialog = $( "#delete-dialog" ).dialog({
+        autoOpen: false,
+        closeOnEscape: false,
+        resizable: false,
+        buttons: dialogButtons,
+        open: function() {
+          progressTimer = setTimeout( progress, 2000 );
+        }
+      }),
+      downloadButton = $( ".delete-btn" )
+        .on( "click", function() {
+          if ($('.main-content-outlet .drop-zone .search-result.active').length){
+            alert("Are you sure you want to delete this object?");
+            dialog.dialog( "open" );
+          } else {
+            alert('Please select an object from the canvas.');
+          }
+        });
+
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( "Current Progress: " + progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "Complete!" );
+        dialog.dialog( "option", "buttons", [{
+          text: "Close",
+          click: closeDownload
+        }]);
+        $(".ui-dialog button").last().focus();
+        $(".main-content-outlet .drop-zone .search-result").each(function(index, el) {
+          var $thisSearchResult = $(this);
+          if ($thisSearchResult.hasClass('active')) {
+            var thisRole = $thisSearchResult.find('.object-title').attr('data-role');
+            $(".main-content-outlet .droppable-list .search-result .object-title")
+              .each(function(index, el) {
+              var $thisTitleItem = $(this);
+              var thisTitleRole = $thisTitleItem.attr('data-role');
+              if (thisTitleRole == thisRole) {
+                $thisTitleItem.closest('.search-result').fadeOut().remove();
+              };
+            });
+            $("#index .search-result .object-title").each(function(index, el) {
+              var $thisTitleItem = $(this);
+              var thisTitleRole = $thisTitleItem.attr('data-role');
+              if (thisTitleRole == thisRole) {
+                $thisTitleItem.closest('.search-result').fadeIn();
+              };
+            });
+            $thisSearchResult.fadeOut().remove();
+          };
+        });
+      }
+    });
+
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+
+      progressbar.progressbar( "value", val + Math.floor( Math.random() * 3 ) );
+
+      if ( val <= 99 ) {
+        progressTimer = setTimeout( progress, 50 );
+      }
+    }
+
+    function closeDownload() {
+      clearTimeout( progressTimer );
+      dialog
+        .dialog( "option", "buttons", dialogButtons )
+        .dialog( "close" );
+      progressbar.progressbar( "value", false );
+      progressLabel
+        .text( "Starting delete..." );
+      downloadButton.focus();
+    }
+  });
+
+  //sync events
+  // $('#sync input').click(function(event) {
+  //   /* Act on the event */
+  //   event.preventDefault();
+  //   var $thisBtn = $(this);
+  //   var thisBtnId = $thisBtn.attr('id');
+  //   var tabId = '#'+thisBtnId +'-sync';
+  //   $('.tab-content .tab-pane.active').removeClass('active');
+  //   $(tabId).addClass('active');
+  // });
 
 });
